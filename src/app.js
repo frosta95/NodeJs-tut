ab3a9a0386d57a87839bdbbf52eed80bab3f4140
Francois Reiland <reilandfourny.francois@gmail.com>
2023-11-02 15:44:45 +0100

    json test finished

--- src/app.js
+++ src/app.js
@@ -1,3 +1,34 @@
-const { v4: uuidv4 } = require("uuid");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-;
+const people = [
+    {
+        "name" : "Caleb",
+        "industry" : "music"
+    },
+    {
+        "name" : "John",
+        "industry" : "networking"
+    },
+    {
+        "name" : "Sully",
+        "industry" : "sports medicine"
+    }
+];
+
+app.get("/", (req, res) =>{
+    res.send("Hello World!");
+})
+
+app.get("/api/customers", (req, res) =>{
+    res.send({"people": people});
+})
+
+app.post("/" , (req, res) =>{
+    res.send("This is a post request!");
+})
+
+app.listen(port, () => {
+   console.log("App listening on port " + port + "...") ;
+})
